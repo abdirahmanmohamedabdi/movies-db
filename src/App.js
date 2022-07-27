94fb40c3ea72d372f83590cb4908d46d54a023f7
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-27 21:22:48 +0300

    My first push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Button from 'react-bootstrap/Button'
+import Navbar from 'react-bootstrap/Navbar'
+function App() {
+  return (
+    <div className="App">
+     
+       <Navbar>this is navbar</Navbar>
+      
+        <Button>This is button</Button> 
+       
+      
+    </div>
+  );
+}
+
+export default App;
