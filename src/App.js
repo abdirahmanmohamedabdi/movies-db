7eba151eb1e5c8ed5a68db41b1f6307af5e483e4
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-27 22:11:04 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-
-import './App.css';
-import Button from 'react-bootstrap/Button'
-import Navbar from 'react-bootstrap/Navbar'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-     
-       <Navbar>this is navbar</Navbar>
-      
-        <Button>This is button</Button> 
-       
-      
+      <header>
+        <Navbar expand="lg" variant="light" bg="light">
+          <Container>
+            <Navbar.Brand href="#">Movies-db</Navbar.Brand>
+          </Container>
+        </Navbar>
+      </header>
     </div>
   );
 }
-
 export default App;
