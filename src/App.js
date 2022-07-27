1e99be6b2905e17674e20093f5e5c03470e8efef
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-28 02:41:17 +0300

    checking to see if data from json was diplaying

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import MovieCard from "./Movie";
 import { Navbar, Container } from "react-bootstrap";
+import Movie from './MoviesData'
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
         </Navbar>
       </header>
       <MovieCard />
+      <Movie />
     </div>
   );
 }
