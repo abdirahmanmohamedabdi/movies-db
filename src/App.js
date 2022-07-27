c98e62b24dc71ca93dcc8ec1e20c5bc304f1db05
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-28 02:02:02 +0300

    removed card

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Container,Card,Button } from "react-bootstrap";
+import MovieCard from "./Movie";
+import { Navbar, Container } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
       <header>
-        
         <Navbar expand="lg" variant="dark" bg="dark">
           <Container>
             <Navbar.Brand href="#">Movies-db</Navbar.Brand>
           </Container>
         </Navbar>
       </header>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+      <MovieCard />
     </div>
   );
 }
