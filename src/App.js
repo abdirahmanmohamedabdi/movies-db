f695d3386d41dff53e1f1727c1e732d6242facfe
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-29 02:09:04 +0300

    added reviewform component to apps.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container } from "react-bootstrap";
 // import Movie from './MoviesData'
 import MovieCtx from './components/MovieCtx'
+import SearchBar from "./components/Search";
+import PostReivew from "./components/PostReivew";
 
 function App() {
   return (
     <div className="App">
       <header>
+        
         <Navbar expand="lg" variant="dark" bg="dark">
+          
           <Container>
+            
             <Navbar.Brand href="#">Movies-db</Navbar.Brand>
+            <label>Search BAR</label>
           </Container>
         </Navbar>
       </header>
       <main>
         <MovieCtx />
+        <PostReivew/>
       </main>
       {/* <MovieCard/> */}
       {/* <Movie/>  */}
