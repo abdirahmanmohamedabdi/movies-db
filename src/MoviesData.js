b4ffedb860f6d9e66805155dc2b756ae1e7bffdd
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-28 21:13:29 +0300

    fixed data error

--- src/MoviesData.js
+++ src/MoviesData.js
@@ -1,21 +1,22 @@
-import React, { useState,useEffect } from "react";
- import MovieCard from "./Movie";
+import React, { useState, useEffect } from "react";
+// import MovieCard from "./Movie";
 function Movie() {
-  const apiUrl = 'localhost:3000/movies' 
+  const apiUrl = "http://localhost:3000/movies";
   let [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch(apiUrl)
-    .then(response => response.json())
-    .then(responseData => {
-      console.log(responseData)
-    //  setMovies(responseData)     
-    }) 
-  },[]);
+      .then((response) => response.json())
+      .then((responseData) => {
+        //console.log(responseData)
+        setMovies(responseData)
+      });
+  }, []);
   return (
     <div className="movies-Data">
-      
-        
-      
+      {/* render a list of <Poem> components in here */}
+      {movies.map((movie, i) => {
+        return <Movie movie={movie} />;
+      })}
     </div>
   );
 }
