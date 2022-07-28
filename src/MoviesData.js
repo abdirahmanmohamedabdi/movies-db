45a151243fcf2bc6769e98b9ed7f5683115c3848
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-28 17:56:34 +0300

    fetchin data from json

--- src/MoviesData.js
+++ src/MoviesData.js
@@ -1,34 +1,23 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState,useEffect } from "react";
+ import MovieCard from "./Movie";
 function Movie() {
-
-    const apiUrl = 'http://localhost:3000/search'
-    let [movies, setMovies] = useState([]);
-    useEffect(() => {
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(responseData => {
-                //    console.log(responseData)
-                  setMovies(responseData)     
-            })
-    }, []);
-
-    return (
-
-        <div className="movies-data">
-
-            {movies.map((movie, i) => {
-
-                return (<Movie movie={movie} />)
-
-            })}
-
-
-        </div>
-    )
-
-
+  const apiUrl = 'localhost:3000/movies' 
+  let [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(responseData => {
+      console.log(responseData)
+    //  setMovies(responseData)     
+    }) 
+  },[]);
+  return (
+    <div className="movies-Data">
+      
+        
+      
+    </div>
+  );
 }
-
 
 export default Movie;
