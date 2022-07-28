b0d748c01b05959c3fbcdf567aba35009786888d
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-28 17:56:11 +0300

    fixed card

--- src/Movie.js
+++ src/Movie.js
@@ -1,20 +1,26 @@
-import { Card, Button } from "react-bootstrap";
-function MovieCard(){
-return(
-<div>
-<Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Movie title</Card.Title>
-        <Card.Text> Year</Card.Text>
-        <Card.Text> score</Card.Text>
-        <Card.Text> Rating</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
+function MovieCard() {
+//    console.log(setMovies)
+  return (
+    <div>
+      <Row xs={3} md={3} className="g-4">
+        {Array.from({ length: 3 }).map((_, idx) => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+
+              <Card.Title>Movie title:</Card.Title>
+              <Card.Text> Year:</Card.Text>
+              <Card.Text> Score:</Card.Text>
+              <Card.Text> Type:</Card.Text>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
-)
-
+  );
 }
 export default MovieCard;
