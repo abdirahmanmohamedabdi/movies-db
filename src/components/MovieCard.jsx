a052aac0b80325636f9a10899c01f884d4c2c2eb
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-29 11:31:46 +0300

    cleaned up code

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,21 +1,20 @@
-import {Card,Col} from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 
-function MovieCard({movie}) {
+function MovieCard({ movie }) {
+  return (
+    <div>
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={movie.image} />
 
-    return (
-        <div>
-            <Col>
-              <Card>
-                <Card.Img variant="top" src={movie.image} />
-  
-                <Card.Title>Movie Title: {movie.title}</Card.Title>
-                <Card.Text> Release Date:{movie.year}</Card.Text>
-                <Card.Text> Score :{movie.score}</Card.Text>
-                <Card.Text> Type:{movie.type}</Card.Text>
-                <Card.Text> Review:{movie.review}</Card.Text>
-              </Card>
-            </Col>
-      </div>
-    )
+          <Card.Title>Movie Title: {movie.title}</Card.Title>
+          <Card.Text> Release Date:{movie.year}</Card.Text>
+          <Card.Text> Score :{movie.score}</Card.Text>
+          <Card.Text> Type:{movie.type}</Card.Text>
+          <Card.Text> Review:{movie.review}</Card.Text>
+        </Card>
+      </Col>
+    </div>
+  );
 }
-export default MovieCard
+export default MovieCard;
