0becc85fd91d809961e0006644e61d8b0aea175d
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-29 11:34:07 +0300

    changed to jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Navbar, Container } from "react-bootstrap";
+
+function Nav() {
+  return (
+    <div className="App">
+      <header>
+        <Navbar expand="lg" variant="dark" bg="dark">
+          <Container>
+            <Navbar.Brand href="#">Movies-db</Navbar.Brand>
+            <label>Search BAR</label>
+          </Container>
+        </Navbar>
+      </header>
+      <main></main>
+    </div>
+  );
+}
+export default Nav;
