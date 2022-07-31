173ce88948e18cc7118885873cd74337d5d7d268
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-31 22:03:27 +0300

    created new review container

---
+++ src/components/NewReview.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+function NewReview({ movieId }) {
+  const [review, setReview] = useState("");
+
+  function changeHandler(event) {
+    setReview(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    if (review.length > 0) {
+      const newReview = {
+        movieId,
+        review,
+      };
+      
+      fetch('http://localhost:3000/reviews',{
+        method:'post',
+        body: JSON.stringify(newReview),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        
+      })
+      .then(() => {
+        alert('Review posted successfully')
+        setReview("")
+      })
+    }
+  }
+
+  return (
+    <div>
+      
+      
+       <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="review"
+          value={review}
+          onChange={changeHandler}
+        />
+         
+        <button type="Submit">Submit Review</button>
+      </form> 
+  </div>
+  );
+}
+
+export default NewReview;
