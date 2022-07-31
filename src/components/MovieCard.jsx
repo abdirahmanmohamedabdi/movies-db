71ded70b279cd6e73e1fb07bc4de133d4dc6c505
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-31 23:08:20 +0300

    imported reviewxtx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,6 @@
-import { Card, Col, ListGroup} from "react-bootstrap";
-import NewReview  from "./NewReview";
-
-
+import { Card, Col, ListGroup } from "react-bootstrap";
+import NewReview from "./NewReview";
+import ReviewCtx from "./ReviewCtx";
 
 function MovieCard({ movie }) {
   return (
@@ -10,16 +9,14 @@ function MovieCard({ movie }) {
         <Card>
           <Card.Img variant="top" src={movie.image} />
           <ListGroup>
-          <ListGroup.Item>{movie.title}</ListGroup.Item>
-      <ListGroup.Item>{movie.year}</ListGroup.Item>
-      <ListGroup.Item>{movie.score}</ListGroup.Item>
-      <ListGroup.Item>{movie.type}</ListGroup.Item>
-      <ListGroup.Item>{<NewReview movieId={movie.id} />}</ListGroup.Item>
-
-      
-
-          
+            <ListGroup.Item>{movie.title}</ListGroup.Item>
+            <ListGroup.Item>{movie.year}</ListGroup.Item>
+            <ListGroup.Item>{movie.score}</ListGroup.Item>
+            <ListGroup.Item>{movie.type}</ListGroup.Item>
+            <ListGroup.Item>{<NewReview movieId={movie.id} />}</ListGroup.Item>
+            {/* <ListGroup.Item>{[(movie.reviews)]}</ListGroup.Item> */}
           </ListGroup>
+          <ReviewCtx reviews={movie.reviews}/>
         </Card>
       </Col>
     </div>
